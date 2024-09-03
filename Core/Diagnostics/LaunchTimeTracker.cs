a422966a8f4a4aa85245b075b4853051771f1466
LunarEclipse <949886@qq.com>
2024-09-03 17:21:10 +0800

    Update LaunchTimeTracker.cs

--- Core/Diagnostics/LaunchTimeTracker.cs
+++ Core/Diagnostics/LaunchTimeTracker.cs
@@ -2,28 +2,65 @@
 
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
 
+using System;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
 
-namespace Core.Diagnostics
+namespace Luna.Diagnostics
 {
     public class LaunchTimeTracker : MonoBehaviour
     {
-        private static Stopwatch _stopwatch = new();
+        public static readonly Stopwatch stopwatch = new();
+        private static long _elapsedMilliseconds;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         static void CaptureLaunchTime()
         {
-            _stopwatch.Start();
+            stopwatch.Start();
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
-
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        static void AfterAssembliesLoaded()
+        { 
+            Debug.Log($"[LaunchTimeTracker] Assemblies loaded in {stopwatch.ElapsedMilliseconds}ms.");
+            _elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        static void BeforeSplashScreen()
+        {
+            Debug.Log($"[LaunchTimeTracker] Prepare splash screen in {stopwatch.ElapsedMilliseconds - _elapsedMilliseconds}ms.");
+            _elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void BeforeSceneLoad()
+        {
+            Debug.Log($"[LaunchTimeTracker] Splash screen loaded in {stopwatch.ElapsedMilliseconds - _elapsedMilliseconds}ms.");
+            _elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void AfterSceneLoad()
+        {
+            // Debug.Log($"[LaunchTimeTracker] Scene loaded in {_stopwatch.ElapsedMilliseconds - _elapsedMilliseconds}ms.");
+            Debug.Log($"[LaunchTimeTracker] Application launched in {stopwatch.ElapsedMilliseconds}ms from startup.");
+        }
+        
         private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
-            _stopwatch.Stop();
-            Debug.Log($"[LaunchTimeTracker] Application launched in {_stopwatch.ElapsedMilliseconds}ms.");
+            Debug.Log($"[LaunchTimeTracker] Scene {arg0.name} loaded in {stopwatch.ElapsedMilliseconds - _elapsedMilliseconds}ms.");
+            _elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+            var go = new GameObject("LaunchTimeTracker", typeof(LaunchTimeTracker));
+            DontDestroyOnLoad(go);
+        }
+
+        private void Start()
+        {
+            Debug.Log($"[LaunchTimeTracker] First frame rendered in {stopwatch.ElapsedMilliseconds}ms from startup.");
         }
     }
 }
