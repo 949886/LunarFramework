00b6c46e59b11216ca78b2fbdbf435c89221590e
LunarEclipse <949886@qq.com>
2024-10-03 17:07:20 +0800

    Do not reset audio labels.

--- UI/Editor/WidgetAssetMenu.cs
+++ UI/Editor/WidgetAssetMenu.cs
@@ -13,41 +13,41 @@ using Object = UnityEngine.Object;
 
 public class WidgetAssetMenu : EditorWindow
 {
-        #region Widgets
-        
-        private static string PREFAB_CACHE_PATH = Application.temporaryCachePath + "/Widgets/";
-    
-        [MenuItem("Assets/Create/UI Widget Script", false, 70)]
-        public static void AddWidgetScript()
+    #region Widgets
+
+    private static string PREFAB_CACHE_PATH = Application.temporaryCachePath + "/Widgets/";
+
+    [MenuItem("Assets/Create/UI Widget Script", false, 70)]
+    public static void AddWidgetScript()
+    {
+        var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        var widgetName = "Test";
+
+        if (widgetName != null)
         {
-            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
-            var widgetName = "Test";
+            // Create a csharp script
+            var scriptPath = path + "/" + widgetName + ".cs";
 
-            if (widgetName != null)
-            {
-
-                // Create a csharp script
-                var scriptPath = path + "/" + widgetName + ".cs";
-
-                // File.WriteAllText(scriptPath, scriptContent);
-                ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateInstance<OnCreateScript>(), "NewWidget", null, null);
-            }
+            // File.WriteAllText(scriptPath, scriptContent);
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateInstance<OnCreateScript>(), "NewWidget",
+                null, null);
         }
-        
-        internal class OnCreateScript : EndNameEditAction
+    }
+
+    internal class OnCreateScript : EndNameEditAction
+    {
+        private Queue<Action> _actions = new();
+
+        public override void Action(int instanceId, string pathName, string resourceFile)
         {
-            private Queue<Action> _actions = new();
-            
-            public override void Action(int instanceId, string pathName, string resourceFile)
-            {
-                if (!pathName.EndsWith(".cs"))
-                    pathName += ".cs";
-                pathName = AssetDatabase.GenerateUniqueAssetPath(pathName);
-                
-                var fileName = Path.GetFileNameWithoutExtension(pathName);
-                
-                var scriptContent = 
-                    @"
+            if (!pathName.EndsWith(".cs"))
+                pathName += ".cs";
+            pathName = AssetDatabase.GenerateUniqueAssetPath(pathName);
+
+            var fileName = Path.GetFileNameWithoutExtension(pathName);
+
+            var scriptContent =
+                @"
 using Luna.UI;
 using Luna.UI.Navigation;
 using UnityEngine;
@@ -57,62 +57,63 @@ public class " + fileName + @": Widget
     
 }
 ";
-                //ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathName, resourceFile);
-                var monoScript = CreateScriptAssetWithContent(pathName, scriptContent) as MonoScript; // ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathName, resourceFile);
-                ProjectWindowUtil.ShowCreatedAsset(monoScript);
-                
-                // Create a prefab
-                var prefab = new GameObject(fileName);
-                // Type scriptClass = monoScript.GetClass();
-                // prefab.AddComponent(scriptClass);
-                PrefabUtility.SaveAsPrefabAsset(prefab, pathName.Replace(".cs", ".prefab"));
-                DestroyImmediate(prefab);
-                
-                // Save the script path as text to cache directory for further processing
-                var scriptPath = Path.Combine(PREFAB_CACHE_PATH, fileName + ".txt");
-                if (!Directory.Exists(PREFAB_CACHE_PATH))
-                    Directory.CreateDirectory(PREFAB_CACHE_PATH);
-                File.WriteAllTextAsync(scriptPath, pathName);
-            }
-            
-            [DidReloadScripts]
-            private static async void CreatePrefabAfterCompile()
-            {
-                if (!Directory.Exists(PREFAB_CACHE_PATH))
-                    return;
-                
-                // Read all the prefabs in the cache directory and add script component
-                var files = Directory.GetFiles(PREFAB_CACHE_PATH, "*.txt");
-                foreach (var file in files)
-                {
-                    var scriptPath = await File.ReadAllTextAsync(file);
-                    var script = AssetDatabase.LoadAssetAtPath<MonoScript>(scriptPath);
-                    var scriptClass = script.GetClass();
-                    var prefab = PrefabUtility.LoadPrefabContents(scriptPath.Replace(".cs", ".prefab"));
-                    prefab.AddComponent(scriptClass);
-                    EditorUtility.SetDirty(prefab);
-                    File.Delete(file);
-                }
-            }
+            //ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathName, resourceFile);
+            var monoScript =
+                CreateScriptAssetWithContent(pathName,
+                        scriptContent) as
+                    MonoScript; // ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathName, resourceFile);
+            ProjectWindowUtil.ShowCreatedAsset(monoScript);
 
-            public override void Cancelled(int instanceId, string pathName, string resourceFile)
-            {
-                Selection.activeObject = null;
-            }
-            
-            private static Object CreateScriptAssetWithContent(string assetPath, string scriptContent)
-            {
-                string fullPath = Path.GetFullPath(assetPath);
-                File.WriteAllText(fullPath, scriptContent);
+            // Create a prefab
+            var prefab = new GameObject(fileName);
+            // Type scriptClass = monoScript.GetClass();
+            // prefab.AddComponent(scriptClass);
+            PrefabUtility.SaveAsPrefabAsset(prefab, pathName.Replace(".cs", ".prefab"));
+            DestroyImmediate(prefab);
 
-                // Import the asset
-                AssetDatabase.ImportAsset(assetPath);
+            // Save the script path as text to cache directory for further processing
+            var scriptPath = Path.Combine(PREFAB_CACHE_PATH, fileName + ".txt");
+            if (!Directory.Exists(PREFAB_CACHE_PATH))
+                Directory.CreateDirectory(PREFAB_CACHE_PATH);
+            File.WriteAllTextAsync(scriptPath, pathName);
+        }
 
-                return AssetDatabase.LoadAssetAtPath(assetPath, typeof(Object));
+        [DidReloadScripts]
+        private static async void CreatePrefabAfterCompile()
+        {
+            if (!Directory.Exists(PREFAB_CACHE_PATH))
+                return;
+
+            // Read all the prefabs in the cache directory and add script component
+            var files = Directory.GetFiles(PREFAB_CACHE_PATH, "*.txt");
+            foreach (var file in files)
+            {
+                var scriptPath = await File.ReadAllTextAsync(file);
+                var script = AssetDatabase.LoadAssetAtPath<MonoScript>(scriptPath);
+                var scriptClass = script.GetClass();
+                var prefab = PrefabUtility.LoadPrefabContents(scriptPath.Replace(".cs", ".prefab"));
+                prefab.AddComponent(scriptClass);
+                EditorUtility.SetDirty(prefab);
+                File.Delete(file);
             }
         }
-        
 
+        public override void Cancelled(int instanceId, string pathName, string resourceFile)
+        {
+            Selection.activeObject = null;
+        }
 
-        #endregion
+        private static Object CreateScriptAssetWithContent(string assetPath, string scriptContent)
+        {
+            string fullPath = Path.GetFullPath(assetPath);
+            File.WriteAllText(fullPath, scriptContent);
+
+            // Import the asset
+            AssetDatabase.ImportAsset(assetPath);
+
+            return AssetDatabase.LoadAssetAtPath(assetPath, typeof(Object));
+        }
+    }
+
+    #endregion
 }
