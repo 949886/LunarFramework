e815c3b084f9a6a7de37a3bcef2000f0074dfa13
LunarEclipse <949886@qq.com>
2024-12-04 12:01:12 +0800

    Update AnimationStateBehaviour.cs

--- Core/Animation/AnimationStateBehaviour.cs
+++ Core/Animation/AnimationStateBehaviour.cs
@@ -16,6 +16,17 @@ namespace Luna.Core.Animation
         private bool _isTransitioning;
         private AnimationState _animationState;
         
+        public event AnimationStateDelegate OnAnimationStartEvent;
+        public event AnimationStateDelegate OnAnimationExitEvent;
+        public event AnimationStateDelegate OnAnimationEndEvent;
+        public event AnimationStateDelegate OnAnimationFinishEvent;
+        public event AnimationStateDelegate OnAnimationUpdateEvent;
+        public event AnimationStateDelegate OnAnimationInterruptEvent;
+        public event AnimationStateDelegate OnAnimationTransitionInStartEvent;
+        public event AnimationStateDelegate OnAnimationTransitionInEndEvent;
+        public event AnimationStateDelegate OnAnimationTransitionOutStartEvent;
+        public event AnimationStateDelegate OnAnimationTransitionOutEndEvent;
+        
         public abstract void OnAnimationStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
         public abstract void OnAnimationExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
         public abstract void OnAnimationEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
@@ -32,18 +43,66 @@ namespace Luna.Core.Animation
         public abstract void OnAnimationTransitionOutEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
         
         
-        public event AnimationStateDelegate OnAnimationStartEvent;
-        public event AnimationStateDelegate OnAnimationExitEvent;
-        public event AnimationStateDelegate OnAnimationEndEvent;
-        public event AnimationStateDelegate OnAnimationFinishEvent;
-        public event AnimationStateDelegate OnAnimationUpdateEvent;
-        public event AnimationStateDelegate OnAnimationInterruptEvent;
-        public event AnimationStateDelegate OnAnimationTransitionInStartEvent;
-        public event AnimationStateDelegate OnAnimationTransitionInEndEvent;
-        public event AnimationStateDelegate OnAnimationTransitionOutStartEvent;
-        public event AnimationStateDelegate OnAnimationTransitionOutEndEvent;
+        private void _OnAnimationStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationStart(animator, stateInfo, layerIndex);
+            OnAnimationStartEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+
+        private void _OnAnimationExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationExit(animator, stateInfo, layerIndex);
+            OnAnimationExitEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        private void _OnAnimationEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationEnd(animator, stateInfo, layerIndex);
+            OnAnimationEndEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
         
-        public delegate void AnimationStateDelegate(AnimationStateBehaviour sender, Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
+        // Called when the an_imation is fully played.
+        private void _OnAnimationFinish(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationFinish(animator, stateInfo, layerIndex);
+            OnAnimationFinishEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        // Called every frame_ when the animation is playing.
+        private void _OnAnimationUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationUpdate(animator, stateInfo, layerIndex);
+            OnAnimationUpdateEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        private void _OnAnimationInterrupt(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationInterrupt(animator, stateInfo, layerIndex);
+            OnAnimationInterruptEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        private void _OnAnimationTransitionInStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationTransitionInStart(animator, stateInfo, layerIndex);
+            OnAnimationTransitionInStartEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        private void _OnAnimationTransitionInEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationTransitionInEnd(animator, stateInfo, layerIndex);
+            OnAnimationTransitionInEndEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        private void _OnAnimationTransitionOutStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationTransitionOutStart(animator, stateInfo, layerIndex);
+            OnAnimationTransitionOutStartEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
+        
+        private void _OnAnimationTransitionOutEnd(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            OnAnimationTransitionOutEnd(animator, stateInfo, layerIndex);
+            OnAnimationTransitionOutEndEvent?.Invoke(this, animator, stateInfo, layerIndex);
+        }
         
         
         public sealed override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -63,11 +122,11 @@ namespace Luna.Core.Animation
         public sealed override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             // Debug.Log($"Progress of {name}: {progress} {stateInfo.normalizedTime}");
-            OnAnimationUpdate(animator, stateInfo, layerIndex);
+            _OnAnimationUpdate(animator, stateInfo, layerIndex);
 
             if (progress == 0f)
             {
-                OnAnimationStart(animator, stateInfo, layerIndex);
+                _OnAnimationStart(animator, stateInfo, layerIndex);
             }
             
             // Check animation transition start/end.
@@ -77,14 +136,14 @@ namespace Luna.Core.Animation
                 {
                     _isTransitioning = true;
                     _animationState = AnimationState.In;
-                    OnAnimationTransitionInStart(animator, stateInfo, layerIndex);
+                    _OnAnimationTransitionInStart(animator, stateInfo, layerIndex);
                 }
                 else
                 {
                     _isTransitioning = true;
                     _animationState = AnimationState.Out;
-                    OnAnimationExit(animator, stateInfo, layerIndex);
-                    OnAnimationTransitionOutStart(animator, stateInfo, layerIndex);
+                    _OnAnimationExit(animator, stateInfo, layerIndex);
+                    _OnAnimationTransitionOutStart(animator, stateInfo, layerIndex);
                 }
             }
             else if (!animator.IsInTransition(layerIndex) && _isTransitioning)
@@ -92,10 +151,10 @@ namespace Luna.Core.Animation
                 switch (_animationState)
                 {
                     case AnimationState.In:
-                        OnAnimationTransitionInEnd(animator, stateInfo, layerIndex);
+                        _OnAnimationTransitionInEnd(animator, stateInfo, layerIndex);
                         break;
                     case AnimationState.Out:
-                        OnAnimationTransitionOutEnd(animator, stateInfo, layerIndex);
+                        _OnAnimationTransitionOutEnd(animator, stateInfo, layerIndex);
                         break;
                 }
                 _isTransitioning = false;
@@ -106,7 +165,7 @@ namespace Luna.Core.Animation
             // When animation is fully played.
             if (progress >= 1f)
             {
-                OnAnimationFinish(animator, stateInfo, layerIndex);
+                _OnAnimationFinish(animator, stateInfo, layerIndex);
             }
             
         }
@@ -122,5 +181,7 @@ namespace Luna.Core.Animation
             In,
             Out
         }
+        
+        public delegate void AnimationStateDelegate(AnimationStateBehaviour sender, Animator animator, AnimatorStateInfo stateInfo, int layerIndex);
     }
 }
